7ad2b3c147f3c887fcb9103a75e95546122d4293
Kiran-Bramhane123 <kiranbramhane83@gmail.com>
2024-04-08 15:02:02 +0530

    Airplane_Mode creating

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
@@ -5,23 +5,24 @@ frappe.ui.form.on("Airplane Ticket", {
     refresh(frm) {
         
     },
-    validate: function(frm) {
-        // Remove duplicate entries from add-ons
-        let uniqueAddOns = {};
-        frm.doc.add_ons.forEach(function(add_on) {
-            let key = add_on.item;
-            if (!uniqueAddOns[key]) {
-                uniqueAddOns[key] = add_on;
-            }
-        });
-        // Update add-ons with unique entries
-        frm.set_value("add_ons", Object.values(uniqueAddOns));
-    }
+    // validate: function(frm) {
+    //     // Remove duplicate entries from add-ons
+    //     let uniqueAddOns = {};
+    //     frm.doc.add_ons.forEach(function(add_on) {
+    //         let key = add_on.item;
+    //         if (!uniqueAddOns[key]) {
+    //             uniqueAddOns[key] = add_on;
+    //         }
+    //     });
+    //     // Update add-ons with unique entries
+    //     frm.set_value("add_ons", Object.values(uniqueAddOns));
+    // }
 });
 
 frappe.ui.form.on("Airplane Ticket Add-on Item", {
     amount(frm, cdt, cdn) {
         let total = 0;
+        console.log(total);
         frm.doc.add_ons.forEach(function(d) {
             total += parseFloat(d.amount) || 0;
         });
